f6c03690af02bd0aabda84fb9a6e5b1798dacc51
tenzinpalden <tpalden2003@gmail.com>
2023-09-21 16:07:00 -0400

    setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDYocrOqm3R8pdOQhFzzoheNBSf8_R4Obk",
+    authDomain: "clone-cad4e.firebaseapp.com",
+    projectId: "clone-cad4e",
+    storageBucket: "clone-cad4e.appspot.com",
+    messagingSenderId: "867312599288",
+    appId: "1:867312599288:web:e1c7b17609c2e22f193b0d",
+    measurementId: "G-RD0CMR9ZLN"
+  };
